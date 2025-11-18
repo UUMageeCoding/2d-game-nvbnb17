a20d808c7dc70d305a11b2857df31378502124f1
nvbnb17 <Stansfield-b@ulster.ac.uk>
2025-11-18 15:44:51 +0000

    18/11/25

--- Assets/_Game/Code/PlatformerController.cs
+++ Assets/_Game/Code/PlatformerController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class PlatformerController : MonoBehaviour
@@ -12,12 +14,29 @@ public class PlatformerController : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
     
     private Rigidbody2D rb;
+
+    private TrailRenderer _trailRenderer;
     private bool isGrounded;
     private float moveInput;
     
+    [Header("Dashing")]
+    [SerializeField] private float _dashingVelocity = 15.5f;
+
+    [SerializeField] private float _dashingTime = 0.1f;
+
+    private Vector2 _dashingDir;
+
+    private bool _isDashing;
+
+    private bool _canDash = true;
+
+
+     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        _trailRenderer = GetComponent<TrailRenderer>();
         
         // Set to Dynamic with gravity
         rb.bodyType = RigidbodyType2D.Dynamic;
@@ -25,11 +44,12 @@ public class PlatformerController : MonoBehaviour
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
     
-    void Update()
+     private void Update()
     {
         // Get horizontal input
         moveInput = Input.GetAxisRaw("Horizontal");
         
+        Debug.Log("_isDashing  in update " + _isDashing);
         // Check if grounded
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         
@@ -38,9 +58,39 @@ public class PlatformerController : MonoBehaviour
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
+
+        //dash input
+
+        var dashInput = Input.GetButtonDown("Dash");
+        Debug.Log("i Dashed" + dashInput);
+
+        if (dashInput && _canDash)
+        {
+            _isDashing = true;
+            _canDash = false;
+            _trailRenderer.emitting = true;
+            _dashingDir = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+            if (_dashingDir == Vector2.zero)
+            {
+                _dashingDir = new Vector2(transform.localScale.x, y:0);
+            }
+        }
+
+        if (_isDashing)
+        {
+            rb.linearVelocity = _dashingDir.normalized * _dashingVelocity;
+            return;
+        }
+
+        if (isGrounded)
+        {
+            _canDash = true;
+        }
+
+        StartCoroutine(StopDashing());
     }
     
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         // Apply horizontal movement
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
@@ -54,5 +104,13 @@ public class PlatformerController : MonoBehaviour
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
         }
+    }
+
+    private IEnumerator StopDashing()
+    {
+        yield return new WaitForSeconds(_dashingTime);
+        _trailRenderer.emitting = false;
+        _isDashing = false;
+        Debug.Log("in coroutine isdashing" + _isDashing);
     }
 }
